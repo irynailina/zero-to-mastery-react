440d5d8b48e75645204f94ef0eff343a00cfaa2b
iryna.vasylenko <iryna.vasylenko@pm.bet>
2021-01-21 17:03:50 +0200

    mosters finder simple app

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Card} from "../Card/Card";
+import './cardList.styles.css'
+
+export const CardList = (props) => {
+    return (
+        <div className='card-list'>
+            {props.monsters.map(monster => <Card key={monster.id} monster={monster}/>)}
+        </div>
+    )
+}
